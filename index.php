d63379de85371cae3bde40186828197ca95891b9
Tim Mukhin <MuhinTV@gmail.com>
2022-02-21 16:58:27 +0700

    add form registration 2

--- index.php
+++ index.php
@@ -1,26 +1,13 @@
 <?php
-error_reporting(E_ALL);
+$title = "Форма регистрации";
+require __DIR__.'/header.php';
 require_once 'db.php';
+error_reporting(E_ALL);
 ?>
-<!doctype html>
-<html lang="ru">
-<head>
-    <!-- Кодировка веб-страницы -->
-    <meta charset="utf-8">
-    <!-- Настройка viewport -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-RU-Compatible" content="ie=edge">
-    <title>Форма регистрации</title>
 
-     <!-- Bootstrap CSS (Cloudflare CDN) -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-       <!-- Bootstrap Bundle JS (Cloudflare CDN) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/style.css"
-</head>
-<body>
-    <div class = "container mt-4"
+    <div class = "container mt-4">
+        <div class="row">
+            <div class="col">
     <h2>Форма регистрации</h2>
     <form action="check.php" method="post">
         <input type="text" class="form-control" name="login" id="login" placeholder="Введите логин"><br>
--- signup.php
+++ signup.php
@@ -1,4 +1,14 @@
 <?php
+$data = $_POST;
+if (isset($data['do_signup'])) {
+    $errors = array();
+    if (trim($data['login']) == '') {
+        $errors = "Введите логин!";
+    }
+    if (trim($data['email']) = ''){
+        $errors = "Введите Email!";
+    }
+}
 $login = filter_var(trim($_POST['login']), FILTER_SANITIZE_STRING);
 $pass = filter_var(trim($_POST['pass']), FILTER_SANITIZE_STRING);
 $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
